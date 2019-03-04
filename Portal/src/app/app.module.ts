68e997388bc2fe1a0bd4d376fb013e5320f0249e
Jonathan <jonathan.d.robertson1985@gmail.com>
2019-03-04 14:40:49 -0700

    upgraded angular version, started with clean app

--- Portal/src/app/app.module.ts
+++ Portal/src/app/app.module.ts
@@ -1,27 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-
-import { HttpClientModule } from '@angular/common/http';
-import { PortalNavComponent } from './portal-nav/portal-nav.component';
-import { PortalBannerComponent } from './portal-banner/portal-banner.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PortalNavComponent,
-    PortalBannerComponent
+    NavbarComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
